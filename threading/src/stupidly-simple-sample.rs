d7301eace50b53d04bd7b1b31ecbeea902a8fa46
Even Lislebø <e.lislebo@gmail.com>
2014-12-23 22:22:53 +0100

    Collecting thread results

--- threading/src/stupidly-simple-sample.rs
+++ threading/src/stupidly-simple-sample.rs
@@ -1,6 +1,8 @@
 use std::thread::Thread;
+use std::any::Any;
 
 // Runs at about 8 seconds single-thread, should suffice to measure the impact of multiple threads
+// And yes, I know this is a moronic implementation, that's the entire point
 fn stupid_fib(n: int) -> int {
     if n == 0 {
         return 0;
@@ -11,16 +13,34 @@ fn stupid_fib(n: int) -> int {
 }
 
 fn main() {
-    let thread1 = Thread::spawn(move || {
-        println!("{}", stupid_fib(42));
+    let result1 = Thread::spawn(move || {
+        return stupid_fib(42);
     });
-    let thread2 = Thread::spawn(move || {
-        println!("{}", stupid_fib(42));
+    let result2 = Thread::spawn(move || {
+        return stupid_fib(42);
     });
-    let thread3 = Thread::spawn(move || {
-        println!("{}", stupid_fib(42));
+    let result3 = Thread::spawn(move || {
+        return stupid_fib(42);
     });
-    let thread4 = Thread::spawn(move || {
-        println!("{}", stupid_fib(42));
+    let result4 = Thread::spawn(move || {
+        return stupid_fib(42);
     });
+    let mut sum = 0i;
+    match result1.join() {
+        Ok(x) => {sum += x}
+        Err(e) => {println!("Error")}
+    }
+    match result2.join() {
+        Ok(x) => {sum += x}
+        Err(e) => {println!("Error")}
+    }
+    match result3.join() {
+        Ok(x) => {sum += x}
+        Err(e) => {println!("Error")}
+    }
+    match result4.join() {
+        Ok(x) => {sum += x}
+        Err(e) => {println!("Error")}
+    }
+    println!("4 * fib(42) = {}", sum);
 }
