ed41b01f9c38834a7c6ef81e418ef0ecd2d0feec
Even Lislebø <e.lislebo@gmail.com>
2014-12-23 21:46:17 +0100

    Implemented stunningly stupid fib function, this should do...

--- threading/src/stupidly-simple-sample.rs
+++ threading/src/stupidly-simple-sample.rs
@@ -2,6 +2,16 @@ use std::thread::Thread;
 use std::io::{File,BufferedReader};
 use std::time;
 
-fn main() {
+// Runs at about 8 seconds single-thread, should suffice to measure the impact of multiple threads
+fn stupid_fib(n: int) -> int {
+    if n == 0 {
+        return 0;
+    } else if n == 1 {
+        return 1;
+    }
+    stupid_fib(n-1) + stupid_fib(n-2)
+}
 
+fn main() {
+    println!("{}", stupid_fib(42));
 }
