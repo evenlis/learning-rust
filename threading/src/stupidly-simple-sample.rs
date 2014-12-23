01c04935a967745e98d3cc31b174b798939f9017
Even Lislebø <e.lislebo@gmail.com>
2014-12-23 21:33:15 +0100

    Compiler bug fixed, retrying

--- threading/src/stupidly-simple-sample.rs
+++ threading/src/stupidly-simple-sample.rs
@@ -2,14 +2,6 @@ use std::thread::Thread;
 use std::io::{File,BufferedReader};
 use std::time;
 
-fn ascii_sum(word: String) -> int {
-    let mut sum = 0i;
-    for c in word.chars() {
-        sum += c as int;
-    }
-    sum-10
-}
-
 fn main() {
 
 }
